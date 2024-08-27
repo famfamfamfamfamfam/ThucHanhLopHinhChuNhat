65d094192d68784681615937af5e02fad23865cf
BinhTran <fam.out16@gmail.com>
2024-08-27 23:52:20 +0700

    Add cac files bai thuc hanh lop hinh chu nhat

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
+namespace LOP
+{
+    public class Rectangle
+    {
+        public double width { get; set; }
+        public double height { get; set;}
+        public Rectangle()
+        {}
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double GetArea()
+        {
+            double area = width*height;
+            return area;
+        }
+        public double GetPerimeter()
+        {
+            double perimeter = (width + height) * 2;
+            return perimeter;
+        }
+        public string Display()
+        {
+            string rectangleInfo = "Rectangle's width = " + width + "Retangle's height = " + height;
+            return rectangleInfo;
+        }
+    }
+    public class ThucThi
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rectangle's width and height:");
+            double width = Convert.ToDouble(Console.ReadLine());
+            double height = Convert.ToDouble(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine(rectangle.Display());
+            Console.WriteLine("Rectangle's area = "+rectangle.GetArea());
+            Console.WriteLine("Rectangle's perimeter = "+rectangle.GetPerimeter());
+        }
+    }
+}
